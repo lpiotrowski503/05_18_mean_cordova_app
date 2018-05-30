8119602380607473b8be13c0ed51d19007ab6f4c
LukaszPiotrowski <lpiotrowski503@gmail.com>
2018-05-30 12:57:23 +0200

    3. Finished adding note, import all notes, styling front-end

--- client/ng-app/src/app/components/pages/register/register.component.ts
+++ client/ng-app/src/app/components/pages/register/register.component.ts
@@ -11,24 +11,78 @@ export class RegisterComponent implements OnInit {
   nick: string
   password: string
 
-  constructor(private authService: AuthService, private router: Router) {}
-
-  onRegisterSubmit() {
-    const user = {
-      nick: this.nick,
-      password: this.password
-    }
-
-    this.authService.postHttp(user, 'server/register').subscribe(data => {
-      if (data.success) {
-        console.log(data)
-        this.router.navigate(['/login'])
-      } else {
-        console.log(data)
-        this.router.navigate(['/register'])
-      }
-    })
+  label = {
+    header: 'Rejestracja',
+    login: 'Login',
+    password: 'Hasło',
+    submit: 'Zarejestruj'
   }
 
-  ngOnInit() {}
+  message = ['Jesteś', 'Zarejestrowany']
+  registredSuccess = false
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  animationInit() {
+    const register = document.getElementById('page')
+    setTimeout(() => {
+      register.classList.remove('opacity-0')
+      register.classList.add('opacity-1')
+    }, 500)
+  }
+
+  animationPage(router, a, b) {
+    this.message = [a, b]
+    const successMessageContainer = document.getElementById(
+      'successMessageContainer'
+    )
+    const successMessage = document.getElementById('successMessage')
+    const register = document.getElementById('page')
+
+    register.classList.remove('opacity-1')
+    register.classList.add('opacity-0')
+
+    setTimeout(() => {
+      successMessageContainer.classList.remove('hidden')
+      successMessage.classList.remove('opacity-0')
+      successMessage.classList.add('opacity-1')
+    }, 500)
+
+    setTimeout(() => {
+      successMessage.classList.remove('opacity-1')
+      successMessage.classList.add('opacity-0')
+    }, 1500)
+
+    setTimeout(() => {
+      successMessageContainer.classList.add('hidden')
+      this.authService.url = router
+      this.router.navigate([router])
+      register.classList.add('opacity-1')
+    }, 2000)
+  }
+
+  onRegisterSubmit() {
+    document
+      .querySelector('input[type=submit]')
+      .classList.add('animation-click')
+
+    if (this.nick && this.password) {
+      const user = {
+        nick: this.nick,
+        password: this.password
+      }
+
+      this.authService.postHttp(user, 'server/register').subscribe(data => {
+        if (data.success) {
+          this.animationPage('login', 'Jesteś', 'Zarejestrowany')
+        } else {
+          this.animationPage('register', 'Wystąpił', 'Błąd')
+        }
+      })
+    }
+  }
+
+  ngOnInit() {
+    this.animationInit()
+  }
 }
